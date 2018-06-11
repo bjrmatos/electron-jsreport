db3f39ffa022b48d1e0ee8f2af13e52171c3b483
Balasubramanian <ebalasubramanian24@gmail.com>
2018-06-11 13:25:22 +0530

    Updated the repository with new project files

---
+++ main.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const url = require('url')
+const path = require('path')
+var jsreport = require('jsreport')({
+  assets: {
+    allowedFiles: '**/*.*',
+    publicAccessEnabled: true,
+    searchOnDiskIfNotFoundInStore: true,
+    rootUrlForLinks: "http://localhost:5488"
+  }
+});
+var fs = require('fs')
+
+jsreport.init().then(function () {
+}).catch(function (e) {
+  console.error(e.stack)
+  process.exit(1)
+})
+
+ipcMain.on('generateReport', (e, args) => {
+  jsreport.render({
+    template: {
+      content: './print.html',
+      engine: 'jsrender',
+      recipe: 'phantom-pdf'
+    },
+    data: args
+  }).then(function (resp) {
+    fs.writeFileSync('report.pdf', resp.content)
+  });
+})
+let win
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+
+} app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
